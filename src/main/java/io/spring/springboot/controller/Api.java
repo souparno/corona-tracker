f0b64958d76b9fbf05a27df0bb713cbe3f4d0314
souparno <souparno.majumder@gmail.com>
2021-02-07 21:54:20 +0530

    stoping the frontend server before starting it and code syntax organisation

--- src/main/java/io/spring/springboot/controller/Api.java
+++ src/main/java/io/spring/springboot/controller/Api.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.spring.springboot.services.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import io.spring.springboot.services.DataService;
 
 @RestController
 @RequestMapping("/api/v1")

89de9ada9db0108df3fb1ac6b7446e5d628b6e5f
souparno <souparno.majumder@gmail.com>
2021-02-11 01:08:59 +0530

    adding the authcontroller

---
+++ src/main/java/io/spring/springboot/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package io.spring.springboot.repository;
+
+import io.spring.springboot.model.Role;
+import io.spring.springboot.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}

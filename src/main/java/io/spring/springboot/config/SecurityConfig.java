f5e93d6754f1ab909e45e8949dbfe67d60744be0
souparno <souparno.majumder@gmail.com>
2021-02-11 14:58:13 +0530

    creating the user controller

--- src/main/java/io/spring/springboot/config/SecurityConfig.java
+++ src/main/java/io/spring/springboot/config/SecurityConfig.java
@@ -82,11 +82,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                 .permitAll()
-                .antMatchers("/api/auth/**")
+                .antMatchers("/api/v1/auth/**")
                 .permitAll()
-                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
+                .antMatchers("/api/v1/user/checkUsernameAvailability", "/api/v1/user/checkEmailAvailability")
                 .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                .antMatchers(HttpMethod.GET, "/api/v1/users/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
---
+++ src/main/java/io/spring/springboot/controller/UserController.java
@@ -0,0 +1,56 @@
+package io.spring.springboot.controller;
+
+import io.spring.springboot.exception.ResourceNotFoundException;
+import io.spring.springboot.model.User;
+import io.spring.springboot.repository.UserRepository;
+import io.spring.springboot.security.UserPrincipal;
+import io.spring.springboot.security.CurrentUser;
+import io.spring.springboot.payload.UserSummary;
+import io.spring.springboot.payload.UserIdentityAvailability;
+import io.spring.springboot.payload.UserProfile;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+
+        return userProfile;
+    }
+}
